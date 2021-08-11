ff967e2d9683b77c22c5a27a176e9e54762688f3
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-11 15:59:49 +0530

    feat:linecomparison with oops

---
+++ linecomparisonoops/linecomparison.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace linecomparisonoops
+{
+    class linecomparison
+    {
+        static void Main(string[] args)
+        {
+            LineComparison lineComparison = new LineComparison();
+            lineComparison.Display();
+            lineComparison.Comparsion();
+        }
+    }
+}
